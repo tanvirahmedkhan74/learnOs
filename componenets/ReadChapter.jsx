ac0b4fd9e829254ba93158baf253e108c06b5e31
tanvir <tanvirahmedkhan74@gmail.com>
2024-04-27 03:08:59 +0600

    [ADD] PDF VIEW | [ADD] PDF Buttons, styles

--- componenets/ReadChapter.jsx
+++ componenets/ReadChapter.jsx
@@ -1,44 +1,70 @@
 import React from 'react';
-import { StyleSheet, Dimensions, View } from 'react-native';
+import {StyleSheet, Dimensions, View} from 'react-native';
 import Pdf from 'react-native-pdf';
+import FileViewer from 'react-native-file-viewer';
 
 export default function ReadChapter({route}) {
-    const {chapter} = route.params;
-    const path = `file:///home/tanvir/codes/react_native/Apps/learnOs/assets/pdf/${chapter}`;
-    console.log(path);
-    const source = { uri: path, cache: true };
-    return (
-        <View style={styles.container}>
-            <Pdf
-                trustAllCerts={true}
-                source={source}
-                onLoadComplete={(numberOfPages,filePath) => {
-                    console.log(`Number of pages: ${numberOfPages}`);
-                }}
-                onPageChanged={(page,numberOfPages) => {
-                    console.log(`Current page: ${page}`);
-                }}
-                onError={(error) => {
-                    console.log(error);
-                }}
-                onPressLink={(uri) => {
-                    console.log(`Link pressed: ${uri}`);
-                }}
-                style={styles.pdf}/>
-        </View>
-    )
+  const {chapter} = route.params;
+  const paths = {
+    1: require("../assets/pdf/ch1.pdf"),
+    2: require("../assets/pdf/ch2.pdf"),
+    3: require("../assets/pdf/ch3.pdf"),
+    4: require("../assets/pdf/ch4.pdf"),
+    5: require("../assets/pdf/ch5.pdf"),
+    6: require("../assets/pdf/ch6.pdf"),
+    7: require("../assets/pdf/ch7.pdf"),
+    8: require("../assets/pdf/ch8.pdf"),
+    9: require("../assets/pdf/ch9.pdf"),
+    10: require("../assets/pdf/ch10.pdf"),
+    11: require("../assets/pdf/ch11.pdf"),
+    12: require("../assets/pdf/ch12.pdf"),
+    13: require("../assets/pdf/ch13.pdf"),
+    14: require("../assets/pdf/ch14.pdf"),
+    15: require("../assets/pdf/ch15.pdf"),
+    16: require("../assets/pdf/ch16.pdf"),
+    17: require("../assets/pdf/ch17.pdf"),
+    18: require("../assets/pdf/ch18.pdf"),
+    19: require("../assets/pdf/ch19.pdf"),
+    20: require("../assets/pdf/ch20.pdf"),
+    21: require("../assets/pdf/ch21.pdf"),
+    // 22: require("../assets/pdf/ch22.pdf"),
+    // 23: require("../assets/pdf/ch23.pdf"),
+  };
+  
+
+  return (
+    <View style={styles.container}>
+      <Pdf
+      trustAllCerts={false}
+        source={paths[chapter]}
+        onLoadComplete={(numberOfPages, filePath) => {
+          console.log(`Number of pages: ${numberOfPages}`);
+        }}
+        onPageChanged={(page, numberOfPages) => {
+          console.log(`Current page: ${page}`);
+        }}
+        onError={error => {
+          console.log(error);
+        }}
+        // onPressLink={uri => {
+        //   console.log(`Link pressed: ${uri}`);
+        // }}
+        style={styles.pdf}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
+  container: {
     flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'center',
     marginTop: 25,
-},
-pdf: {
-    flex:1,
-    width:Dimensions.get('window').width,
-    height:Dimensions.get('window').height,
-}
+  },
+  pdf: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 });
--- screens/home/Homescreen.jsx
+++ screens/home/Homescreen.jsx
@@ -1,11 +1,12 @@
-import { View, Text, Button } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import {View, Text, Button, StyleSheet, ScrollView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
 import InitialLoading from '../../componenets/InitialLoading';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import WelcomeScreen from './WelcomeScreen';
 import Header from '../../componenets/Header';
 import ReadChapter from '../../componenets/ReadChapter';
+import FileViewer from 'react-native-file-viewer';
 
 export default function Homescreen({route}) {
   const navigation = useNavigation();
@@ -23,7 +24,7 @@ export default function Homescreen({route}) {
       } finally {
         // Set loading to false even if there's an error
         setTimeout(() => {
-          setLoading(false)
+          setLoading(false);
         }, 2000);
       }
     };
@@ -32,24 +33,216 @@ export default function Homescreen({route}) {
   }, [route]);
 
   return (
-    <View style={{flex: 1}}>
+    <View style={styles.container}>
       {loading ? (
         <InitialLoading />
       ) : (
         <>
-          {
-            userName === '' ? (
-              <WelcomeScreen/>
-            ) : (
-              <View style={{flex: 1}}>
-                <Header name={userName}/>
-                <Button title='Chapter 1' onPress={() => navigation.navigate('Read', {chapter: "ch1.pdf"})}/>
-                <Button title='Chapter 2'/>
+          {userName === '' ? (
+            <WelcomeScreen />
+          ) : (
+            <View style={styles.content}>
+              <Header name={userName} />
+              <View style={styles.buttonContainer}>
+                <ScrollView style={styles.scrollView}>
+                  <Button
+                    title="Introduction"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 1});
+                    }}
+                    style={styles.btnStyle}
+                  />
+
+                  <Button
+                    title="Operating System Services"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 2});
+                    }}
+                    style={styles.btnStyle}
+                  />
+
+                  <Button
+                    title="Processes"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 3});
+                    }}
+                    style={styles.btnStyle}
+                  />
+
+                  <Button
+                    title="Threads and Concurrency"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 4});
+                    }}
+                    style={styles.btnStyle}
+                  />
+
+                  <Button
+                    title="CPU Scheduling"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 5});
+                    }}
+                    style={styles.btnStyle}
+                  />
+
+                  <Button
+                    title="Synchronization Tools"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 6});
+                    }}
+                    style={styles.btnStyle}
+                  />
+
+                  <Button
+                    title="Synchronization Examples"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 7});
+                    }}
+                    style={styles.btnStyle}
+                  />
+
+                  <Button
+                    title="Deadlocks"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 8});
+                    }}
+                    style={styles.btnStyle}
+                  />
+
+                  <Button
+                    title="Main Memory"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 9});
+                    }}
+                    style={styles.btnStyle}
+                  />
+
+                  <Button
+                    title="Virtual Memory"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 10});
+                    }}
+                    style={styles.btnStyle}
+                  />
+
+                  <Button
+                    title="Mass-Storage Structure"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 11});
+                    }}
+                    style={styles.button}
+                  />
+
+                  <Button
+                    title="I/O System"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 12});
+                    }}
+                    style={styles.button}
+                  />
+
+                  <Button
+                    title="File-System Interface"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 13});
+                    }}
+                    style={styles.button}
+                  />
+
+                  <Button
+                    title="File-System Implementation"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 14});
+                    }}
+                    style={styles.button}
+                  />
+
+                  <Button
+                    title="File-System Internals"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 15});
+                    }}
+                    style={styles.button}
+                  />
+
+                  <Button
+                    title="Security"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 16});
+                    }}
+                    style={styles.button}
+                  />
+
+                  <Button
+                    title="Protection"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 17});
+                    }}
+                    style={styles.button}
+                  />
+
+                  <Button
+                    title="Virtual Machines"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 18});
+                    }}
+                    style={styles.button}
+                  />
+
+                  <Button
+                    title="Network and Distributed Systems"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 19});
+                    }}
+                    style={styles.button}
+                  />
+
+                  <Button
+                    title="The Linux System"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 20});
+                    }}
+                    style={styles.button}
+                  />
+
+                  <Button
+                    title="Windows 10"
+                    onPress={() => {
+                      navigation.navigate('Read', {chapter: 21});
+                    }}
+                    style={styles.button}
+                  />
+                </ScrollView>
               </View>
-            )
-          }
+            </View>
+          )}
         </>
       )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0', // Light background
+  },
+  content: {
+    flex: 1, // Allow content to fill remaining space
+  },
+  buttonContainer: {
+    flex: 1, // Allow buttons to take up remaining space after header
+  },
+  scrollView: {
+    flex: 1, // Allow ScrollView to fill available space within buttonContainer
+  },
+  btnStyle: {
+    // Adjust button styles here
+    backgroundColor: '#007bff', // Primary blue color
+    padding: 15,
+    borderRadius: 5,
+    color: '#fff', // White text color
+    width: '100%', // Make buttons full width
+    marginVertical: 10, // Add margin between buttons
+  },
+});
